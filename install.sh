7a8dab0b53193ff33c59b2d27079bbeceef9e4a0
Andrew Scala <agscala@gmail.com>
2020-10-09 11:54:59 -0400

    Improving nvim setup automation

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 cd ~/.vim
 
+# curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+           # https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp ~/.vim/init.vim ~/.config/nvim/init.vim
+
 vim +'PlugInstall --sync' +qa
 mkdir -p ~/.config/nvim
 
-ln -s ~/.vim/vimrc ~/.config/nvim/init.vim
 
